414c729f8e8e40581408af76f930e4070f06791a
Dmytro Kravchuk <dimakravchuck93@gmail.com>
2022-07-13 11:04:09 +0300

    refactor routing

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,30 +1,41 @@
-import React, { FC } from "react";
+import React, { FC, FunctionComponent } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 import { LOGIN_ROUTE, MAIN_PAGE_ROUTE } from "../contstants";
 import { useAppSelector } from "../hooks/redux";
 import { privateRoutes, publicRoutes } from "../routes";
 
+type RoutesProps = {
+  path: string;
+  Component: FunctionComponent;
+  children?: RoutesProps[];
+};
+
 export const Router: FC = () => {
   const { isAuth } = useAppSelector((state) => state.authReducer);
 
+  const getRoutes = (routes: Array<RoutesProps>) => {
+    // ToDo: doesn't work for nested path, need to refactor
+    return routes.map((route: RoutesProps) => {
+      const { path, Component, children } = route;
+      if (children) {
+        return (
+          <React.Fragment key={path}>
+            <Route path={path} element={<route.Component />}>
+              {getRoutes(children)}
+            </Route>
+          </React.Fragment>
+        );
+      }
+      return <Route path={path} element={<Component />} key={path} />;
+    });
+  };
+
   return (
     <Routes>
-      {isAuth ? (
-        <>
-          {privateRoutes.map(({ path, Component }) => (
-            <Route path={path} element={<Component />} key={path} />
-          ))}
-        </>
-      ) : (
-        <>
-          {publicRoutes.map(({ path, Component }) => (
-            <Route path={path} element={<Component />} key={path} />
-          ))}
-        </>
-      )}
+      {isAuth ? getRoutes(privateRoutes) : getRoutes(publicRoutes)}
       <Route
-        path='*'
+        path='*' // ToDo: doesn't work for nested path, need to refactor
         element={
           isAuth ? (
             <Navigate to={MAIN_PAGE_ROUTE} />
